c80db6652ee1b58d2861f11c9dc4c880fa82d3c2
Alexei Dudko <axo3@ya.ru>
2020-10-20 17:29:45 +0300

    feat: add task completion code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort (twoDimArr) {
+    
+    twoDimArr = twoDimArr || [];
+    let towel = [];
+
+    twoDimArr.forEach( (arr, arrIndex) => {
+    	if (arrIndex % 2 !== 0) {
+        	// разворачиваем массив
+            let stack = [];
+            arr.forEach( arrItem => {
+                // используя стек
+            	stack.push(arrItem);
+            });
+            while (true) {
+            	let next = stack.pop();
+                if (!next) break;
+                else towel.push(next);
+            }
+        } else {
+        	towel = towel.concat(arr)
+        }
+    })
+    return towel;
 }
